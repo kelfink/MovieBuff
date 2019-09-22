1ee2105299d896647b332fb7cdefa5ed1be7ad24
Kevin Fries <kelfink@yahoo.com>
2019-09-22 08:12:27 -0700

    trim the dataframe title for better lookup

--- src/main/scala/com/moviechallenge/Movie.scala
+++ src/main/scala/com/moviechallenge/Movie.scala
@@ -2,6 +2,7 @@ package com.moviechallenge
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.{DataFrame, SparkSession, Row}
+import org.apache.spark.sql.functions._
 //import org.apache.spark.sql.types._
 
 import org.apache.spark.sql.SQLContext
@@ -16,21 +17,17 @@ class Movie {
       .getOrCreate()
     import spark.implicits._
 
-    //val sc = spark.sparkContext
-   /* val spark: SparkSession = SparkSession
-      .builder()
-      .appName("Spark SQL basic example")
-      .config("spark.some.config.option", "some-value")
-      .getOrCreate()
-*/
-
     val df: DataFrame = spark.read.format("org.apache.spark.csv")
         .option("header", true)
         .option("inferSchema", true)
         .csv("data/the-movies-dataset/movies_metadata.csv")
 
-      //df.filter($"age"===10)
-      val movies =  df.filter($"title".like("%" + title + "%"))
+       val trimmedDF = df.withColumn(
+        "trimmed_title",
+        trim(col("title"))
+      )
+      //val movies =  df.filter($"title".like("%" + title + "%"))
+      val movies =  trimmedDF.filter($"trimmed_title".===(title))
       movies.show()
       val foundtitle = movies.first().getAs[String]("title")
       //val selectedData: Unit = df.filter("trim(title) = 'Plan 9 From Outer Space'").show()
